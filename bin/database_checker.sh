0329871f90a3c9635f96d6ef5be0baa2a45a00b2
Nick <nvx4@cdc.gov>
2022-04-21 12:25:12 -0400

    Inital commit for to check and update required non-included databases

---
+++ bin/database_checker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -l
+
+#$ -o database_checker.out
+#$ -e database_checker.err
+#$ -N database_checker
+#$ -cwd
+#$ -q short.q
+
+#
+# Description: Script checks for file tag to see if database version is the same. If it is behind it will download the new set
+#
+# Usage ./database_checker.sh path_to_database_folder
+#
+# Modules required: None
+#
+# v1.0.0 (04/21/2022)
+#
+# Created by Nick Vlachos (nvx4@cdc.gov)
+#
+
+do_download="false"
+# Checks for proper argumentation
+if [[ $# -eq 0 ]]; then
+	echo "No argument supplied to $0, exiting"
+	exit 113
+elif [[ "${1}" = "-h" ]]; then
+	echo "Usage ./database_checker.sh path_to_config_file [-i]"
+	echo "-i is too install databases, otherwise script just checks for existence"
+	exit 0
+elif [[ ! -d "${1}" ]]; then
+	echo "No folder exists as ${1}...exiting"
+	exit 113
+else
+	path_to_DBs="${1}"
+fi
+
+# Shows where databases should be (installed)
+echo "${path_to_DBs}"
+
+if [[ ! -f "${path_to_DBs}/DB_versions.txt" ]]; then
+	do_update="true"
+else
+	local_DB_version=$(tail -n1 "${path_to_DBs}/DB_versions.txt" | cut -d'-' -f1)
+	echo "Downloading latest database version file (wget ftp://ftp.cdc.gov/pub/QUAISAR-FTP/DB_versions.txt)"
+	wget -O "${path_to_DBs}/DB_versions_remote.txt" "ftp://ftp.cdc.gov/pub/QUAISAR-FTP/DB_versions.txt"
+	remote_DB_version=$(tail -n1 "${path_to_DBs}/DB_versions_remote.txt" | cut -d'-' -f1)
+	if [[ "${local_DB_version}" = "${remote_DB_version}" ]]; then
+		echo "Databases are up to date!"
+	else
+		echo "Databases need to be updated"
+		do_update="true"
+	fi
+fi
+
+if [[ "${do_download}" = "true" ]]; then
+	echo "Downloading latest database version file (wget ftp://ftp.cdc.gov/pub/QUAISAR-FTP/DB_versions.txt)"
+	wget -O "${path_to_DBs}/DB_versions_remote.txt" "ftp://ftp.cdc.gov/pub/QUAISAR-FTP/DB_versions.txt"
+	remote_DB_version=$(tail -n1 "${path_to_DBs}/DB_versions_remote.txt" | cut -d'-' -f1)
+fi
+
+if [[ "${do_update}" = "true" ]]; then
+	remote_DB_version=$(tail -n1 "${path_to_DBs}/DB_versions_remote.txt" | cut -d'-' -f1)
+	echo "Downloading latest database version file (wget ftp://ftp.cdc.gov/pub/QUAISAR-FTP/${remote_DB_version}.tar.gz)"
+	wget -O "${path_to_DBs}/DB_version_${remote_DB_version}.txt" "ftp://ftp.cdc.gov/pub/QUAISAR-FTP/${remote_DB_version}.tar.gz"
+	gzip -zvxf "${path_to_DBs}/DB_version_${remote_DB_version}.txt"
+fi
